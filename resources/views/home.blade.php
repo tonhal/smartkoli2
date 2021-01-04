8fa3fb6a3edcbaec165078addc32bed8da192d07
tonhal <57263132+tonhal@users.noreply.github.com>
2021-01-04 17:32:46 +0100

    Vue Bootstrap Setup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,25 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <example-component></example-component>
     </div>
-</div>
-@endsection
+</body>
+</html>
